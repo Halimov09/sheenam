7ed69b53b8b0974199d7cbf1d9c8844510a906ed
halimov09 <sunnatullohhalimov@gmail.com>
2024-10-11 23:59:24 +0500

    Brokers: Add Insert

--- Sheenam.Api/Program.cs
+++ Sheenam.Api/Program.cs
@@ -3,6 +3,8 @@
 // Free To Use Comfort and Peace
 //==================================================
 
+using FluentAssertions.Common;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
