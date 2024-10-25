2102f24b107ec270247b38a914b755011fcaed33
halimov09 <sunnatullohhalimov@gmail.com>
2024-10-25 23:36:17 +0500

    ADD: New Controller -> GuestsController

---
+++ Sheenam.Api/Controllers/GuestsController.cs
@@ -0,0 +1,29 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Microsoft.AspNetCore.Mvc;
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Services.Foundations.Guests;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestsController : Controller
+    {
+        private IGuestService guestService;
+
+        public GuestsController(IGuestService guestService)
+        {
+            this.guestService = guestService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostGuest(Guest guest)
+        {
+            return Ok(await this.guestService.AddGuestAsync(guest));
+        }
+    }
+}
